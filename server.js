832d957c84ccc1970b11480fdedb4b2f00ce2827
JP <jpjcjbr@gmail.com>
2016-08-07 21:40:40 -0300

    initial version

---
+++ server.js
@@ -0,0 +1,40 @@
+var port = 9000
+var express = require('express')
+var request = require('request')
+var app = express()
+ 
+app.get('/top-users', function (req, res) {
+  request('http://localhost:8080/tweets/top-users', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	res.end(response.body)
+	  } else {
+	  	console.error(err.stack);
+  		res.status(500).send('Internal error');
+	  }
+	})
+})
+
+app.get('/tweet-count-by-hashtag-and-language', function (req, res) {
+  request('http://localhost:8080/tweets/tweet-count-by-hashtag-and-language', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	res.end(response.body)
+	  } else {
+	  	console.error(err.stack);
+  		res.status(500).send('Internal error');
+	  }
+	})
+})
+
+app.get('/tweet-count-by-hour', function (req, res) {
+  request('http://localhost:8080/tweets/tweet-count-by-hour', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	res.end(response.body)
+	  } else {
+	  	console.error(err.stack);
+  		res.status(500).send('Internal error');
+	  }
+	})
+})
+ 
+app.listen(port)
+console.log("Tweets-api started on port " + port)
